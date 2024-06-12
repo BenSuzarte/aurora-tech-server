edc81ace2c21327c67f03ecf8eebc371a5e3961e
BenSuzarte <ben.dantassuzarte@gmail.com>
2024-06-12 00:27:42 -0300

    feat: adjusted functions and responses to client

--- src/controllers/Jobs/controller.ts
+++ src/controllers/Jobs/controller.ts
@@ -3,8 +3,25 @@ import JobsService from '@/services/Jobs/service';
 
 class JobController {
   async createJob(req: Request, res: Response) {
-    console.log(req.body, "\n", req.params.idUsuario)
-    const validate = await JobsService.createJob(req.body, req.params.idUsuario);
+    const validate = await JobsService.createJob(req.body, req.body.idUsuario);
+
+    if (!validate) {
+      return res.status(500).json({
+        message: 'Erro ao criar o job'
+      });
+    }
+
+    if (validate.code === 404) {
+      return res.status(404).json({
+        message: 'Não foi possível criar sua nova vaga'
+      });
+    }
+
+    if (validate.code === 200) {
+      return res.status(201).json({
+        message: 'Job criado com sucesso'
+      });
+    }
   }
 }
 
